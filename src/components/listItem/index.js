0e94bc1af16b52a218dd48d4816f721369db11ec
chisom5 <okoyechisom54@gmail.com>
2021-04-02 01:05:34 +0100

    worked on the pagination component

--- src/components/listItem/index.js
+++ src/components/listItem/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 const ListItem = (props) => {
+  const splittedDate = props.listContent.pushed_at.split("T");
+
   return (
     <div className="list__container">
       <div className="list__avatar">
@@ -9,11 +11,13 @@ const ListItem = (props) => {
 
       <section className="list__content">
         <h2>{props.listContent.name}</h2>
-        <p>{props.listContent.description}</p>
+        <p id="desc">{props.listContent.description}</p>
         <span className="list__content_otherdescription">
-            <div className='stargazers'>{props.listContent.stargazers_count}</div>
-            <div>{props.listContent.open_issues_count}</div>
-            <p></p>
+          <div className="stargazers">{props.listContent.stargazers_count}</div>
+          <div>{props.listContent.open_issues_count}</div>
+          <p>
+            Submitted on {splittedDate[0]} by {props.listContent.name}
+          </p>
         </span>
       </section>
     </div>
--- src/pages/home/components/index.js
+++ src/pages/home/components/index.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
+import { Pagination } from "antd";
 import Header from "../../../components/header";
 import ListItem from "../../../components/listItem";
 import "../../style.scss";
 
 class HomeComponent extends Component {
+  state = {
+    current: 1,
+  };
   componentDidMount() {
     this.props.fetchTrendingRepos(1);
   }
+  onChange = (page) => {
+    this.setState(
+      {
+        current: page,
+      },
+      () => {
+        this.props.fetchTrendingRepos(page);
+      }
+    );
+  };
+
   render() {
     return (
       <div className="init__container">
         <Header />
         <div className="mini__container">
+          <Pagination
+            className="paginated"
+            current={this.state.current}
+            onChange={this.onChange}
+            total={
+              this.props.trendingReposArr &&
+              this.props.trendingReposArr.total_count
+            }
+          />
           <section className="main__content">
             {this.props.trendingReposArr &&
               this.props.trendingReposArr.items.map((chat, i) => {
